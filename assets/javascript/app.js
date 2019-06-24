88c13410f5fc8e8080fdf0b9f75ac4e0e6f1e84a
HH964 <49995995+HH964@users.noreply.github.com>
2019-06-24 10:29:03 -0500

    updated readme

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+var state = "";
+var name = ""; 
+var 
+
+
+var queryURL = "https://api.openbrewerydb.org/breweries";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response){});
