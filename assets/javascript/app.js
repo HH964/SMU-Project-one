61788a5dad3f495f516335a665a18b00c1f122c2
Adrian Cervantes <lagos86@hotmail.com>
2019-06-25 10:27:16 -0500

    backgroun

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,17 +3,38 @@ $("#find-brewery").on("click", function(event) {
 
     event.preventDefault();
     
-    
-    var city = $("#brewery-input").val().trim();
-    
-       var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city;   
+    $("#brewery-view").empty();
+   var state = $("#brewery-input-state").val().trim();
+
+   var city = $("#brewery-input-city").val().trim();
+     
+   $("#brewery-input-state").val("");
+   $("#brewery-input-city").val("");
+
+    //    var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city;  
+       
+       var queryURL = "https://api.openbrewerydb.org/breweries?by_state=" + state +"&by_city=" + city; 
+        
+        //  console.log(queryURL)
            
           $.ajax({
             url: queryURL,
             method: "GET"
           }).then(function(response) {
-            console.log(response);
-            $("#brewery-view").text(JSON.stringify(response));
+              var result = response;
+              for (var i = 0; i < result.length; i++){
+              var location1 = result[i].latitude;
+              var location2 = result[i].longitude;
+              
+            console.log("this is your location: " + location1,location2);
+            if (location1 && location2) {
+                $("#brewery-view").append("<p>" + location1 + " " + location2 + "</p>") 
+            }
+            
+              }
+              
           });
         
         });
+
+ 
